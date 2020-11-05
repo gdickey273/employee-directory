5320cbb36fa7d066c6e69d3af5df2a195474b445
gdickey273 <gdickey273@gmail.com>
2020-11-05 13:53:26 -0500

    created structure for class component

---
+++ src/pages/BandMembers/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+
+class BandMembers extends Component {
+  state = {
+
+  }
+  
+  
+  render () {
+    return (
+      <>
+      </>
+    )
+  }
+}
+
+export default BandMembers;
