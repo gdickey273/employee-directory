8b2ffce20b5918cf2a957002bbb955c601ae301a
gdickey273 <gdickey273@gmail.com>
2020-11-05 10:43:15 -0500

    coded basic Navbar functionality

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-light">
+      <Link className="navbar-brand" to="/">
+        The Collection
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/about"
+                ? "nav-link-active"
+                : "nav-link"
+              }
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/band-members"
+              className={
+                window.location.pathname === "/band-members" ? "nav-link-active" : "nav-link"
+              }
+            >
+              Band Members
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
