c1fcfd1f559d581b23419f061e93406d371839da
gdickey273 <gdickey273@gmail.com>
2020-11-05 11:30:45 -0500

    created basic About page with ImgCard

---
+++ src/pages/About/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ImgCard from "../components/ImgCard";
+
+function About () {
+  return (
+    <ImgCard src="assets/images/beautiful-life.jpg"/>
+  )
+}
+
+export default About;
