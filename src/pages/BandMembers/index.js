1b0d482f1a481108277bd775a6321538d69d345a
gdickey273 <gdickey273@gmail.com>
2020-11-05 14:38:58 -0500

    added FilterForm to BandMembers component

--- src/pages/BandMembers/index.js
+++ src/pages/BandMembers/index.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
 
+import FilterForm from "../../components/FilterForm";
+
 class BandMembers extends Component {
   state = {
-
+    bandMembers: [],
+    filter: ""
   }
   
+  handleInputChange = (event) => {
+    this.setState({ filter: event.target.value}); 
+    console.log(this.state.filter);
+  }
   
   render () {
     return (
       <>
+        <FilterForm 
+          handleInputChange = {this.handleInputChange}
+        />
       </>
     )
   }
