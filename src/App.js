a76a698014adb2c1928ba0026ecf5327ed75dca0
gdickey273 <gdickey273@gmail.com>
2020-11-05 14:39:47 -0500

    added Route for '/band-members'

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./pages/About"
+import BandMembers from "./pages/BandMembers";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Route exact path="/" component={About} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/band-members" component={BandMembers} />
       </div>
     </Router>
   );
