5f5977cc0e972175af5f5e3746ae3f52f3a2457c
gdickey273 <gdickey273@gmail.com>
2020-11-05 14:38:02 -0500

    created form component to filter table

---
+++ src/components/FilterForm/index.js
@@ -0,0 +1,28 @@
+import React, { Component} from "react";
+
+class FilterForm extends Component {
+  
+  render () {
+    return (
+      <>
+      <form className="filter">
+        <div className="form-group">
+          <label htmlFor="name">Filter Table By Member Name</label>
+          <input
+            value={this.props.filter}
+            onChange={this.props.handleInputChange}
+            name="name"
+            type="text"
+            className="form-control"
+            placeholder="Band Member Name"
+            id="name"
+          />
+        </div>
+      </form>
+      </>
+    )
+  }
+  
+}
+
+export default FilterForm;
