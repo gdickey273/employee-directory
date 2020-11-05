24243b09d58e60796645f1c10e6877be9a1eca83
gdickey273 <gdickey273@gmail.com>
2020-11-05 11:30:09 -0500

    edited component to accept src prop for background image

--- src/components/ImgCard/index.js
+++ src/components/ImgCard/index.js
@@ -2,11 +2,11 @@
 import React from "react"
 import "./style.css";
 
-function ImgCard() {
+function ImgCard(props) {
   return (
     <a href="https://linktr.ee/collectionband">
       <div className="card bg-light text-black">
-        <img className="card-img" src="assets/images/beatiful-life.jpg" alt="Beatiful Life Acoustic Artwork" />
+        <img className="card-img" src={`${props.src}`} alt="Beatiful Life Acoustic Artwork" />
         <div className="card-img-overlay">
           <h5 className="card-title">Beautiful Life Acoustic Out Now!</h5>
           <p className="card-text">Listen here.</p>
